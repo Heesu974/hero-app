c48f97679e480876177fb8659df554b503615230
i-mnam <66421797+Heesu974@users.noreply.github.com>
2023-01-01 22:52:30 +0900

    This,more reasonable way as asynchronous for heroService result

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -8,18 +9,14 @@ import { HEROES } from './mock-heroes';
 export class HeroService {
   constructor() {}
 
-  getHeroes(): Hero[] {
-    return HEROES;
+  // getHeroes(): Hero[] {
+  //   return HEROES;
+  // }
+  getHeroes(): Observable<Hero[]> {
+    const ObservableHeroes = of(HEROES);
+
+    return ObservableHeroes;
   }
 }
 
-//@Injectable() 데코레이터는 service를 위한 metadata object를 갖는다. 이는 component 클래스의 @Component() 데코레이터와 같다.
-
-//HeroService는 hero data를 어디에서든 가져 올 수 있다. web service ok, local storage ok, mock data source ok,
-//여기서는 직접 제작한 HROES 즉, mock data를 이용한다.
-
-//getHeroes() method를 통해 mock 데이타를 반환한다.
-
-//기본적으로 ng generate service는 provider를 root injector에 등록한다. provider 메타데이터를 포함함으로써. 이게 providedIn: 'root'가 되는거.
-
-
+//of(HEROES) returns an Observable<Hero[]> that emits a single value, the array of mock heroes.
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
 
@@ -14,11 +14,19 @@ export class HeroesComponent {
   //   this.heroes = this.heroService.getHeroes();
   // }
 
-  ngOnInit(): void {
-    // this.getHeroes();
-    this.heroes = this.heroService.getHeroes();
-  }
+  // ngOnInit(): void {
+  //   // this.getHeroes();
+  //   this.heroes = this.heroService.getHeroes();
+  // }
 
+  getHeroes(): void {
+    this.heroService
+      .getHeroes()
+      .subscribe((ObservableHeroes) => (this.heroes = ObservableHeroes));
+  }
+  ngOnInit(): void {
+    this.getHeroes();
+  }
   selectedHero?: Hero;
   onSelect(hero: any) {
     this.selectedHero = hero;
@@ -28,3 +36,6 @@ export class HeroesComponent {
 //getHeroes() 메서드를 추가함으로써, service로부터 heroes를 되찾습니다.
 
 //getHeroes() 를 ngOnInit lifecycle hook에서 돌아가게 한다. 앵귤러가 적절한 타임에 ngOnInit()을 실행할 수 있도록.
+
+//중요한거, -이거 때문에 한참 헤맸음
+//Call getHeroes() inside the ngOnInit lifesycle hook and let Angular call ngOnInit() at an appropriate time, after constructing a HeroesComponent instance.
