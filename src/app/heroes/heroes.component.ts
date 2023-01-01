3b495051ac38048279e7ac800303dec3dff2585d
i-mnam <66421797+Heesu974@users.noreply.github.com>
2023-01-01 21:33:47 +0900

    This, synchronous way for heroService result

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,9 +8,23 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent {
-  heroes = HEROES;
+  heroes: Hero[] = [];
+  constructor(private heroService: HeroService) {}
+  // getHeroes(): void {
+  //   this.heroes = this.heroService.getHeroes();
+  // }
+
+  ngOnInit(): void {
+    // this.getHeroes();
+    this.heroes = this.heroService.getHeroes();
+  }
+
   selectedHero?: Hero;
   onSelect(hero: any) {
     this.selectedHero = hero;
   }
 }
+
+//getHeroes() 메서드를 추가함으로써, service로부터 heroes를 되찾습니다.
+
+//getHeroes() 를 ngOnInit lifecycle hook에서 돌아가게 한다. 앵귤러가 적절한 타임에 ngOnInit()을 실행할 수 있도록.
