8c46409077150dc9c3470c55846523189b0d16d8
DESKTOP-V4BT0E4\cnt_24 <heesu.kang95@gmail.com>
2023-01-02 09:31:19 +0900

    Add messageservice

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,21 +2,36 @@ import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';
+import {MessageService} from './message.service';
+import { PropertyRead } from '@angular/compiler';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HeroService {
-  constructor() {}
+  constructor(private messageService: MessageService) {}
 
   // getHeroes(): Hero[] {
   //   return HEROES;
   // }
   getHeroes(): Observable<Hero[]> {
     const ObservableHeroes = of(HEROES);
-
+    this.messageService.add('HeroService: fetched heroes');
+    //이때, heroes가 fetch되면, getHeroes() method는 message를 messageService의 add method의 parameter로 넣는다.
     return ObservableHeroes;
   }
 }
 
-//of(HEROES) returns an Observable<Hero[]> that emits a single value, the array of mock heroes.
+//**of(HEROES) returns an Observable<Hero[]> that emits a single value, the array of mock heroes.
+
+
+//***먼저, service-in-service 를 구현하기 위해서, messageService를 HeroesComponent에 장착된 HeroService에 장착합니다. 
+//constructor의 parameter에 private로 messageService property를 선언합니다.
+//Angular는 HeroService가 생성될 때, MessageService개체를 프로퍼티에 장착합니다. 
+
+//****Send a message from HeroService
+// this.messageService.add('HeroService: fetched heroes');
+    //이때, heroes가 fetch되면, getHeroes() method는 message를 messageService의 add method의 parameter로 넣는다.
+
+//*****Display the message from HeroService
+//the MessagesComponent should display all messages, HeroService에서 보내는 메세지를 포함한 모든 메세지.
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,23 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
-
+import {MessageService} from '../message.service'
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent {
-  heroes: Hero[] = [];
-  constructor(private heroService: HeroService) {}
-  // getHeroes(): void {
-  //   this.heroes = this.heroService.getHeroes();
-  // }
 
-  // ngOnInit(): void {
-  //   // this.getHeroes();
-  //   this.heroes = this.heroService.getHeroes();
-  // }
+  selectedHero?: Hero;
+
+  heroes: Hero[] = [];
+  constructor(private heroService: HeroService, private messageService: MessageService) {}
 
   getHeroes(): void {
     this.heroService
@@ -27,15 +22,10 @@ export class HeroesComponent {
   ngOnInit(): void {
     this.getHeroes();
   }
-  selectedHero?: Hero;
+  
   onSelect(hero: any) {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 }
 
-//getHeroes() 메서드를 추가함으로써, service로부터 heroes를 되찾습니다.
-
-//getHeroes() 를 ngOnInit lifecycle hook에서 돌아가게 한다. 앵귤러가 적절한 타임에 ngOnInit()을 실행할 수 있도록.
-
-//중요한거, -이거 때문에 한참 헤맸음
-//Call getHeroes() inside the ngOnInit lifesycle hook and let Angular call ngOnInit() at an appropriate time, after constructing a HeroesComponent instance.
