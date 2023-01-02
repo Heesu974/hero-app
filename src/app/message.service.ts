aba687f471c78ae96885613c1df47166de08be74
DESKTOP-V4BT0E4\cnt_24 <heesu.kang95@gmail.com>
2023-01-02 17:26:28 +0900

    중간 저장

--- src/app/message.service.ts
+++ src/app/message.service.ts
@@ -16,8 +16,5 @@ export class MessageService {
   constructor() { }
 }
 
-//이 서비스는 messages의 cache와 두 개의 method를 배출합니다.
-//하나는, message를 cache 에 추가하는 것이고,
-//다른 하나는 cache를 지우는 것이다.
+//Add navigation with routing
 
-//이 서비스를 HeroService에 장착(inject)합니다.
