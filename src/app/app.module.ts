59802095a1de07874f4b16d4615da186b94678e8
DESKTOP-V4BT0E4\cnt_24 <heesu.kang95@gmail.com>
2023-01-03 16:37:11 +0900

    HTTP 서비스 구축과 데이터 서버 목킹하기

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
-import { FormsModule } from '@angular/forms';
+import { FormControlDirective, FormsModule } from '@angular/forms';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
+//1.HTTP 서비스 추가하기
+import {HttpClientModule}from '@angular/common/http';
+
+//2. 데이터 서버 목킹하기 - in-memory web api 로드
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
+
 @NgModule({
   declarations: [AppComponent, HeroesComponent, HeroDetailComponent, MessagesComponent, DashboardComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  imports: [
+    BrowserModule, 
+    AppRoutingModule, 
+    FormsModule, 
+    HttpClientModule, 
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, {dataEncapsulation: false})],
   providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
 
-//appModule이 RouterModule을 export하는  ApproutingModule을 import하기 때문에. 
-//routerOutlet이라는 router directives를 AppComponent에서 사용할 수 있는 것이다.
+//forRoot() method는 inmemorydataservice를 인자로 받아서 인 메모리 데이터베이스의 실행환경을 구성한다.
+
 
 
---
+++ src/studywithme.md
@@ -0,0 +1,8 @@
+# Meaning of ...
+
+## Spread Operator of Typescript
+
+https://basarat.gitbook.io/typescript/future-javascript/spread-operator
+
+https://stackoverflow.com/questions/46235436/angular-whats-the-meaning-of-these-three-dots-in-ngrx
+
